16704fcdae395b4777fd887f5a3b685b0cc99f22
satvik1257 <83113815+satvik1257@users.noreply.github.com>
2021-09-11 09:44:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+prediction_1="";
+Webcam.set({
+    width:350,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapsot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+    });
+}
+console.log('ml5',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/wTAgFzcxG/model.json',modelLoaded);
+function modelLoaded()
+{
+console.log("model loaded successfully");
+}
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data_1="The first prediction is"+prediction_1;
+    
+    speak_data_2="and the second prediction is"+prediction_2;
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utterThis);
+
+}
+function check()
+{
+  img=document.getElementById("captured_image");
+  classifier.classify(img,got_result);
+}
+function got_result(error,results)
+{
+if(error)
+{
+console.error(error);
+}
+else{
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML=results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+    prediction_1=results[0].label;
+    prediction_2=results[1].label;
+    speak();
+    if(results[0].label == "Amazing")
+    {
+    document.getElementById("result_emoji").innerHTML="&#128076;";
+    document.getElementById("captured_image").innerHTML='<img id="captured_image" src="'+ - This is looking Amazing +'">';
+    }
+    if(results[0].label == "Best")
+    {
+    document.getElementById("result_emoji").innerHTML="&#128077;";
+    document.getElementById("captured_image").innerHTML='<img id="captured_image" src="'+ -All the best +'">';
+    }
+    if(results[0].label == "Victory")
+    {
+    document.getElementById("result_emoji").innerHTML="&#9996;";
+    document.getElementById("captured_image").innerHTML='<img id="captured_image" src="'+ - That was a marvelous victory +'">';
+    }
+
+}
+}
